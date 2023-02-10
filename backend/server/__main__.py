38fbdd8a896a1f384ec543673505b13a21165d40
Str1kez <vazinov@yandex.ru>
2023-02-10 22:22:53 +0300

    🔨 Frontend has been added

--- backend/server/__main__.py
+++ backend/server/__main__.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from uvicorn import run
 
 from server.config import DefaultSettings
@@ -11,7 +12,22 @@ def bind_routes(app: FastAPI):
         app.include_router(router, prefix=path_prefix)
 
 
+def add_cors(app: FastAPI):
+    origins = [
+            "*",
+            ]
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+
 app = FastAPI()
+add_cors(app)
 bind_routes(app)
 
 
